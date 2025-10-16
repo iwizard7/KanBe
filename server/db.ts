bad8a5d2bc22a2a4ceef0718e4f2c26c80b8bc06
Dmitry <35992274+iwizard7@users.noreply.github.com>
2025-10-16 13:31:51 +0700

    Исправлен путь к БД в server/db.ts: ./kanbe.db -> ./data/kanbe.db

--- server/db.ts
+++ server/db.ts
@@ -3,5 +3,5 @@ import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import * as schema from "@shared/schema";
 
-const sqlite = new Database('./kanbe.db');
+const sqlite = new Database('./data/kanbe.db');
 export const db = drizzle({ client: sqlite, schema });
